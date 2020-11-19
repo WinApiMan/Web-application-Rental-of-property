16297bfb3f4eee5813eb668beb1fb22b29af9902
WinApiMan <61493290+WinApiMan@users.noreply.github.com>
2020-11-20 02:57:46 +0300

    Added localization

---
+++ RentalOfProperty.WebUserInterface/RentalOfProperty/Controllers/Settings/SettingsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RentalOfProperty.Controllers.Settings
+{
+    public class SettingsController : Controller
+    {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            const int YearsForSettings = 1;
+
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions
+                {
+                    Expires = DateTimeOffset.UtcNow.AddYears(YearsForSettings)
+                }
+            );
+
+            return LocalRedirect(returnUrl);
+        }
+    }
+}
--- RentalOfProperty.WebUserInterface/RentalOfProperty/Startup.cs
+++ RentalOfProperty.WebUserInterface/RentalOfProperty/Startup.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Globalization;
 
 namespace RentalOfProperty
 {
@@ -19,7 +21,27 @@ namespace RentalOfProperty
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            //Add localization
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            services.AddControllersWithViews()
+                .AddDataAnnotationsLocalization()
+                .AddViewLocalization();
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                const string English = "en", Russian = "ru";
+
+                var supportedCultures = new[]
+                {
+                new CultureInfo(English),
+                new CultureInfo(Russian),
+                };
+
+                options.DefaultRequestCulture = new RequestCulture(Russian);
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
 
             services.AddDistributedMemoryCache();
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
@@ -43,6 +65,9 @@ namespace RentalOfProperty
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
+
+            app.UseRequestLocalization();
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
